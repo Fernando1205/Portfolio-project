9f54aae31b9624f64f443c0f55843df17ffe16ad
Fernando Lopez <fer68769@gmail.com>
2022-03-01 12:01:05 -0600

    Guardar proyecto a api

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -13,6 +13,7 @@ export class CreateComponent implements OnInit {
 
   public title: string='';
   public project: Project;
+  public status: string = '';
 
   constructor(
     private projectService: ProjectService
@@ -25,6 +26,18 @@ export class CreateComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    this.projectService.saveProject(this.project).subscribe(
+      response => {
+        console.log(response);
+        if(response.project){
+          this.status = 'success';
+        } else {
+          this.status = 'failed';
+        }
+      },
+      error => {
+        console.log(<any>error)
+      }
+    )
   }
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from "rxjs";
-import { Project } from "../models/project";
+import { Project } from '../models/project';
 import { Global } from "./global";
 
 @Injectable()
@@ -14,7 +14,10 @@ export class ProjectService {
     this.url = Global.url;
   }
 
-  testService(){
-    return 'Probando servicio';
+  saveProject(project: Project): Observable<any>{
+    let params = JSON.stringify(project);
+    let headers = new HttpHeaders().set('Content-Type','application/json');
+
+    return this._http.post(this.url+'save-project', params, { headers: headers});
   }
 }
