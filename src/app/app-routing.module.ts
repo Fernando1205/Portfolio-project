ebab0a8a6f6ebef38c09872c3cbaba4af867fada
Fernando Lopez <fer68769@gmail.com>
2022-02-28 18:22:45 -0600

    Nuevos componentes y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { AppComponent } from './app.component';
+
+// COMPONENTES
+import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path:'about', component: AboutComponent },
+  { path:'projects', component: ProjectsComponent },
+  { path:'create-project', component: CreateComponent },
+  { path:'contact', component: ContactComponent },
   { path: '**', component: HomeComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AboutComponent } from './about/about.component';
+import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
