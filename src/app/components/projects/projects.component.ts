7bb3e25733fb1c0acc1d18c7610513cfe8c5f282
Fernando Lopez <fer68769@gmail.com>
2022-03-01 18:17:06 -0600

    se muestran proyectos de la bd

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  providers: [ProjectService]
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  public projects: Project[] = [];
+
+  constructor(
+    private projectService: ProjectService
+  ) {
+    this.projectService.getProjects().subscribe(
+      response => {
+        if(response){
+          this.projects = response.project;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
